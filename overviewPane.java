aa72f9e3e268a741c4e5fd3ce830ffecb725964a
reimerw2000 <114110598+reimerw2000@users.noreply.github.com>
2022-09-24 17:09:17 -0400

    an update to the current GUI
    Merging the current function GUI with working light/dark with an GUI with a basic menu and other miscellaneous features

---
+++ overviewPane.java
@@ -0,0 +1,25 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class overviewPane {
+    
+    private BorderPane bp = new BorderPane();
+    private GridPane gp = new GridPane();
+    private Label testLabel = new Label("This is a label");
+    private Button buttonTwo = new Button("Button");
+    private Button testButton = new Button("Button");
+    private TextField testTF = new TextField("Enter info");
+    private Scene guiComponentScene;
+}
+
+
+
