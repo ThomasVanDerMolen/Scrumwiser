af2b46ce6410fdcd5c7ddd5424f0cae4a8f0aef8
reimerw2000 <114110598+reimerw2000@users.noreply.github.com>
2022-10-31 13:44:49 -0400

    Added code for template folder and file adding.

---
+++ docTemplate.java
@@ -0,0 +1,42 @@
+import javafx.scene.shape.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+public class docTemplate extends Object {
+
+    public void open() throws IOException
+    {
+        /* 
+        Desktop.getDesktop().open(new File("C:\\folder"));
+        File file = new File("C:\\folder\\test.txt");
+        System.out.println("test");
+        boolean result;  
+
+try   
+{  
+result = file.createNewFile();  //creates a new file  
+if(result)      // test if successfully created a new file  
+{  
+System.out.println("file created "+file.getCanonicalPath()); //returns the path string  
+}  
+else  
+{  
+System.out.println("File already exist at location: "+file.getCanonicalPath());  
+}  
+}   
+catch (IOException e)   
+{  
+e.printStackTrace();    //prints exception if any  
+}       
+  */
+}  
+}  
+
+    
+   
+
+
